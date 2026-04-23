7dfda1a5e2bf15a8d697929dce8358be1e98e60d
Adriane M Hanson <ahansonuga@gmail.com>
2026-04-23 14:52:55 -0400

    Expand comments

--- metadata-csv.py
+++ metadata-csv.py
@@ -16,7 +16,7 @@ import os
 import sys
 
 
-# Variables from script arguments.
+# Assigns script arguments to variables, including providing default for er_number if no argument provided.
 aips_directory = sys.argv[1]
 collection_id = sys.argv[2]
 try:
@@ -24,7 +24,8 @@ try:
 except IndexError:
     er_number = 1
 
-# Calculate department (ARCHive group) based on the collection id.
+# Calculates the department (ARCHive group) based on the collection id.
+# Quits the script with an explanation if it does not match one of the expected patterns.
 if collection_id.startswith('harg') or collection_id.startswith('ua'):
     department = 'hargrett'
 elif collection_id.startswith('rbrl'):
@@ -33,16 +34,18 @@ else:
     print('Collection ID is not an expected pattern. Should start with harg, ua, or rbrl')
     sys.exit(1)
 
-# Starts metadata.csv with a header row in aips_directory.
+# Creates metadata.csv with a header row in aips_directory.
 with open(os.path.join(aips_directory, 'metadata.csv'), 'w', newline='') as md_csv:
     md_write = csv.writer(md_csv)
     md_write.writerow(['Department', 'Collection', 'Folder', 'AIP_ID', 'Title', 'Version'])
 
-    # For each folder in pres_dir, calculates aip_id and saves row to metadata.csv.
+    # For each folder in aips_directory, calculates the aip_id and saves row to metadata.csv.
     # All other values are the same for every row.
     for folder_name in os.listdir(aips_directory):
         if folder_name == 'metadata.csv':
             continue
         aip_id = f'{collection_id}-er-{er_number:06}'
         md_write.writerow([department, collection_id, folder_name, aip_id, 'TitleTBD', 1])
+
+        # Adds one to the er_number to be ready for making the next AIP ID.
         er_number += 1
