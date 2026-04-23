8758b3e618a7bebd7f6b3ac9d6f124c3d222ee20
Adriane M Hanson <ahansonuga@gmail.com>
2026-04-23 14:21:26 -0400

    Variables from arguments

--- metadata-csv.py
+++ metadata-csv.py
@@ -10,3 +10,21 @@ import csv
 import os
 import sys
 
+
+# Variables from script arguments.
+pres_copy = sys.argv[1]
+coll_id = sys.argv[2]
+try:
+    start_num = sys.argv[3]
+except IndexError:
+    start_num = 1
+
+# Calculate group based on the collection id.
+if coll_id.startswith('harg') or coll_id.startswith('ua'):
+    group = 'hargrett'
+elif coll_id.startswith('rbrl'):
+    group = 'russell'
+else:
+    print('Collection ID is not an expected pattern. Should start with harg, ua, or rbrl')
+    sys.exit(1)
+
