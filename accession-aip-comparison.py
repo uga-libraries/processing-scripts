dfc3bd0927a03c927c17eaa27ddd9c46f4c18b28
Adriane M Hanson <ahansonuga@gmail.com>
2026-04-23 16:57:36 -0400

    Find and combine accession manifests

--- accession-aip-comparison.py
+++ accession-aip-comparison.py
@@ -14,6 +14,24 @@ import os
 import pandas as pd
 import sys
 
+
+def accession_manifests(coll_folder):
+    """Find every accession bag manifest in the collection folder and combine to one df"""
+    df_combined = pd.DataFrame(columns=['MD5', 'Path'])
+    for folder_name in os.listdir(coll_folder):
+        # Determines if the folder is an accession.
+        if folder_name.lower().endswith('er') or folder_name.endswith('no-acc-num'):
+            manifest_path = os.path.join(coll_folder, folder_name, f'{folder_name}_bag', 'manifest-md5.txt')
+            try:
+                df = pd.read_csv(manifest_path, delimiter='  data/', engine='python', header=None, names=['MD5', 'Path'])
+                df_combined = pd.concat([df_combined, df], ignore_index=True)
+            except FileNotFoundError:
+                print(f'{manifest_path} not found')
+
+    print(df_combined['Path'])
+    return df_combined
+
+
 if __name__ == '__main__':
 
     # Assign arguments to variables and calculate parent of aips_directory for saving the report.
@@ -22,6 +40,7 @@ if __name__ == '__main__':
     output_directory = os.path.dirname(aips_directory)
 
     # Find the accession bag manifests in the collection_folder and combine to one dataframe.
+    df_accession = accession_manifests(collection_folder)
 
     # Find the AIP bag manifests in the aips_directory and combine into one dataframe.
 
