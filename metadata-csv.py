ce9cd51fc8f2227a4fd9e1b64e3c9d1bfeb930ca
Adriane M Hanson <ahansonuga@gmail.com>
2026-04-23 14:24:01 -0400

    Change start_num to er_num
    Variable name has to keep making sense as add one to it to get the current sequential number.

--- metadata-csv.py
+++ metadata-csv.py
@@ -4,7 +4,7 @@ Create a metadata.csv file for a folder ready to be turned into AIPs.
 Parameters:
     pres_copy (required): path to PreservationCopy directory, which contains the folders to be made into AIPs
     coll_id (required): collection identifier
-    start_num (optional): first sequential number to use for the AIP ID, if not 1
+    er_num (optional): first sequential number to use for the AIP ID, if not 1
 """
 import csv
 import os
@@ -15,9 +15,9 @@ import sys
 pres_copy = sys.argv[1]
 coll_id = sys.argv[2]
 try:
-    start_num = sys.argv[3]
+    er_num = sys.argv[3]
 except IndexError:
-    start_num = 1
+    er_num = 1
 
 # Calculate group based on the collection id.
 if coll_id.startswith('harg') or coll_id.startswith('ua'):
