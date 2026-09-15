4d301f7720fd5bc639867bf5593910635ac8f416
Adriane M Hanson <ahansonuga@gmail.com>
2026-09-15 15:02:13 -0400

    Add test for matching multiple accessions/aips

--- tests/accession_aip_comparison/test_accession_aip_comparison.py
+++ tests/accession_aip_comparison/test_accession_aip_comparison.py
@@ -22,6 +22,20 @@ class MyTestCase(unittest.TestCase):
             if os.path.exists(report):
                 os.remove(report)
 
+    def test_match_multiple(self):
+        """Test for when there are two accessions, two AIPs, the accessions include duplicates,
+        and all AIP MD5 + filenames match at least one row in the accession"""
+        # Makes variables for the script arguments and runs the script.
+        collection_folder = os.path.join('tests', 'accession_aip_comparison', 'match_multiple', 'collection')
+        aips_directory = os.path.join('tests', 'accession_aip_comparison', 'match_multiple', 'aips_dir')
+        message = subprocess.run(f'python accession-aip-comparison.py {collection_folder} {aips_directory}',
+                                 shell=True, stdout=subprocess.PIPE)
+
+        # Tests the correct message is printed. It would not print if a report was made.
+        result = message.stdout.decode('utf-8')
+        expected = "AIP fixity is unchanged\r\n"
+        self.assertEqual(expected, result, "Problem with test for match_multiple")
+
     def test_match_single(self):
         """Test for when there is one accession, one AIP, and all AIP MD5 + filenames match one row in the accession"""
         # Makes variables for the script arguments and runs the script.
